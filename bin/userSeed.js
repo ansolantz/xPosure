8be6ae43b31264ee3600b318e1511199f18f90a6
Ann-Sofi Lantz <Anso@Anso.local>
2019-05-03 13:02:40 +0200

    seeds

--- bin/userSeed.js
+++ bin/userSeed.js
@@ -8,42 +8,31 @@ mongoose.connect(`mongodb://localhost/${dbName}`);
 
 const user = [
   {
-    firstName: "Sara",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    author: "Suzanne Collins",
-    rating: 10
-  },
-  {
-    title: "Harry Potter",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    author: "J.K. Rowling ",
-    rating: 9
-  },
+    firstName: "Test",
+    lastName: "Test",
+    email: "String",
+    username: "String",
+    passwordHash: "String",
+    profile: {
+      bio: "String",
+      image: "String",
+      location: "String"
+    },
+    media: [],
+    followers: [],
+    following: [],
+    likes: [],
+    saved: [],
+    timestamp: {
+      joined: Date,
+      activityLog: []
+    }
+  }
+
 ]
 
 
 
-const userSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  username: String,
-  passwordHash: String,
-  profile: {
-    bio: String,
-    image: String,
-    location: String
-  },
-  media: [],
-  followers: [],
-  following: [],
-  likes: [],
-  saved: [],
-  timestamp: {
-    joined: Date,
-    activityLog: []
-  }
-});
 
 User.create(user, (err) => {
   if (err) { throw (err) }
