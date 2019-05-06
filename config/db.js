a1b916a30f2f959ae07460e53af5591f5166f096
ansolantz <48385990+ansolantz@users.noreply.github.com>
2019-05-06 17:36:22 +0200

    Photo view page (#7)
    First version of photo view

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const dbName = 'xposure';
-
 const dbUrl = `mongodb+srv://xposure:xposure@cluster0-4xs44.mongodb.net/${dbName}?retryWrites=true`;
 
 module.exports = { dbName, dbUrl };
