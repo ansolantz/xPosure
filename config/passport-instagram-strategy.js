1e2fe0931a385d5143b3f4e166887494ca26facf
Scott Mallinson <scott@scottmallinson.com>
2019-05-08 10:17:33 +0200

    Instagram callback URL amend

--- config/passport-instagram-strategy.js
+++ config/passport-instagram-strategy.js
@@ -6,7 +6,8 @@ const InstagramStrategy = require('passport-instagram').Strategy;
 passport.use(new InstagramStrategy({
   clientID: 'feef7763763f446e98b94c6caf272f42',
   clientSecret: 'e94d86e57aa04ab5a8faaf29d28e2ba0',
-  callbackURL: 'http://localhost:3000/auth/instagram/callback'
+  // callbackURL: 'http://localhost:3000/auth/instagram/callback'
+  callbackURL: 'https://xposure-ironhack.herokuapp.com'
 },
 (accessToken, refreshToken, profile, done) => {
   let user = {};
